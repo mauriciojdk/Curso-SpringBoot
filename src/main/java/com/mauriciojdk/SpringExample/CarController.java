487c5d0fbcd907082dd58b5e698a5f309d44997c
Mauricio <mauricio@pop-os.localdomain>
2022-05-27 23:07:43 -0300

    Curso finalizado

--- src/main/java/com/mauriciojdk/SpringExample/CarController.java
+++ src/main/java/com/mauriciojdk/SpringExample/CarController.java
@@ -25,6 +25,24 @@ public class CarController {
        return this.carRepository.save(car);
     }
 
+    @DeleteMapping("/{id}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void removeById(@PathVariable("id") Long id){
+        this.carRepository.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void updateById(@PathVariable("id") Long id, @RequestBody Car car){
+        this.carRepository.findById(id).map(record -> {
+            record.setName(car.getName());
+            record.setColor(car.getColor());
+            record.setYear(car.getYear());
+            Car updated = this.carRepository.save(record);
+            return updated;
+        });
+    }
+
     @GetMapping
     @ResponseBody
     public List<Car> findAll(){
